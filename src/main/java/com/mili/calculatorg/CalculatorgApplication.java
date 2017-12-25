77f0d281846d7ad74f127dab9adba91f505a696f
Dragan Milicevic <dmilicevic@gmx.de>
2017-12-25 16:10:14 +0100

    Add Spring Boot skeleton

---
+++ src/main/java/com/mili/calculatorg/CalculatorgApplication.java
@@ -0,0 +1,12 @@
+package com.mili.calculatorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorgApplication.class, args);
+	}
+}
